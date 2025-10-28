f4d083d9c31a26cea26db9f98058a68aa474ced4
Parth Patel <pparth230@gmail.com>
2025-10-27 21:51:51 -0400

    Added readme file

--- image_processing.py
+++ image_processing.py
@@ -14,14 +14,17 @@ class image_classifier():
         return self.image
     
     def grayscale (self): # independent on the self.image
+        """Converts the image into gray scale extracting specific channels"""
         gray = 0.299 * self.image[:, :, 0] + 0.587 * self.image[:, :, 1] + 0.114 * self.image[:, :, 2] 
         return gray.astype(np.uint8)
     
     def invert (self): # independent on the self.image
-        inverted = 255 -self.image
+        """Converts the image into values"""
+        inverted = 255 -self.image # get the counter value of the existing 
         return inverted.astype(np.uint8)
     
     def threshold(self, value):
+        """Converts the pixels into black and white depending on the threshold values"""
         binary = np.where(self.image > value, 255, 0)
         return binary.astype(np.uint8)
     
@@ -33,8 +36,8 @@ class image_classifier():
         kernel = np.array([[0, -1, 0],
                        [-1, 5, -1],
                        [0, -1, 0]])
-        sharpened = cv2.filter2D(self.image, -1, kernel)
-        return sharpened.astype(np.uint8)
+        sharpened = cv2.filter2D(self.image, -1, kernel) # -1 is the convention of saying match the input with the output
+        return sharpened.astype(np.uint8) # returns the value in the integer from any floats/decimals
         
     def save_image(self, filepath, image_array=None):
         if image_array is None:
